305b1d204bd39d39c56d4aeb2491d85742724780
awaik <awaiking44@gmail.com>
2021-02-11 08:01:45 +0300

    First commit

---
+++ example/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:iphone_has_notch/iphone_has_notch.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('This phone is iPhone and it has notch', style: TextStyle(fontSize: 16, color: Colors.black),),
+              Text('${IphoneHasNotch.hasNotch}', style: TextStyle(fontSize: 30, color: Colors.blue[800]),),
+            ],
+          ),
+        ),
+
+    );
+  }
+}
---
+++ lib/iphone_has_notch.dart
@@ -0,0 +1,26 @@
+library iphone_has_notch;
+
+import 'dart:io';
+import 'dart:ui' as ui;
+
+class IphoneHasNotch {
+  /// Returns true in case it is iPhone & it has notch.
+
+  static bool get hasNotch {
+    if (Platform.isIOS &&
+        (ui.window.physicalSize.height == 812 ||
+            ui.window.physicalSize.height == 812 ||
+            ui.window.physicalSize.height == 896 ||
+            ui.window.physicalSize.height == 896 ||
+            // iPhone 12 pro
+            ui.window.physicalSize.height == 844 ||
+            ui.window.physicalSize.height == 844 ||
+            // Iphone 12 pro max
+            ui.window.physicalSize.height == 926 ||
+            ui.window.physicalSize.height == 926)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
