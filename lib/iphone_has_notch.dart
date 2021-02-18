2066c0dd412f9cb51554fcaa6bc18412ee665999
awaik <awaiking44@gmail.com>
2021-02-18 09:11:37 +0300

    fixed detection bug

--- lib/iphone_has_notch.dart
+++ lib/iphone_has_notch.dart
@@ -7,16 +7,20 @@ class IphoneHasNotch {
   /// Returns true in case it is iPhone & it has notch.
   static bool get hasNotch {
     if (Platform.isIOS &&
-        (ui.window.physicalSize.height == 812 ||
-            ui.window.physicalSize.height == 812 ||
-            ui.window.physicalSize.height == 896 ||
-            ui.window.physicalSize.height == 896 ||
+        (ui.window.physicalSize.height.toInt() == 812 ||
+            ui.window.physicalSize.height.toInt() == 812 * 2 ||
+            ui.window.physicalSize.height.toInt() == 812 * 3 ||
+            ui.window.physicalSize.height.toInt() == 896 ||
+            ui.window.physicalSize.height.toInt() == 896 * 2 ||
+            ui.window.physicalSize.height.toInt() == 896 * 3 ||
             // iPhone 12 pro
-            ui.window.physicalSize.height == 844 ||
-            ui.window.physicalSize.height == 844 ||
+            ui.window.physicalSize.height.toInt() == 844 ||
+            ui.window.physicalSize.height.toInt() == 844 * 2 ||
+            ui.window.physicalSize.height.toInt() == 844 * 3 ||
             // Iphone 12 pro max
-            ui.window.physicalSize.height == 926 ||
-            ui.window.physicalSize.height == 926)) {
+            ui.window.physicalSize.height.toInt() == 926 ||
+            ui.window.physicalSize.height.toInt() == 926 * 2 ||
+            ui.window.physicalSize.height.toInt() == 926 * 3)) {
       return true;
     } else {
       return false;
